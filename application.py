1ccb6952056450bcda10e77a79c97742c53b09bc
Dman0808 <168228320+Dman0808@users.noreply.github.com>
2024-10-24 15:31:54 +0000

    Update Flask import and route in application.py

--- application.py
+++ application.py
@@ -4,4 +4,10 @@ app = Flask(1.1.2)
 from app import app
 @app.route("C:User/dus08/OneDrive/Documents/Github/AutoGPT/autogpt_platform/autogpt_libs/pyproject.toml")
 def hello():
+    """
+    Returns a greeting message.
+
+    Returns:
+        str: A greeting message "Hello World!".
+    """
     return "Hello World!"
