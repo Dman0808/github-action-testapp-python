55bf87cd38cef5d30cc67bce88d2ad633edb4dd4
Dman0808 <168228320+Dman0808@users.noreply.github.com>
2024-10-24 15:26:16 +0000

    Update Flask import and route in application.py

---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import Flask
+
+
+app = Flask(1.1.2)
--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(1.1.2)
 
+from app import app
 @app.route("C:User/dus08/OneDrive/Documents/Github/AutoGPT/autogpt_platform/autogpt_libs/pyproject.toml")
 def hello():
     return "Hello World!"
